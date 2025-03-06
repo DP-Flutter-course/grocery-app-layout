eb0ec1b9f4756c6ce0a05c26e866e7aa31bf6c9f
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-03-06 18:31:33 +0530

    container

--- grocery_app/lib/pages/homePage.dart
+++ grocery_app/lib/pages/homePage.dart
@@ -14,6 +14,39 @@ class HomePage extends StatelessWidget {
         shadowColor: Colors.black,
         elevation: 3,
       ),
+
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                height: 50,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: Colors.grey.withOpacity(0.5),
+                  border: Border.all(
+                    color: Colors.blueGrey,
+                    width: 1,
+                  )
+                ),
+
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15),
+                      child: Icon(Icons.search, size: 35, color: Colors.blueGrey,),
+                    ),
+
+                    TextField()
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
